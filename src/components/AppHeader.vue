f8b056a423f5231f5edc9af83024b5b5cf868bd7
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-04-08 14:55:56 +0200

    add :root palette + add header static layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,11 +7,38 @@
 <template>
     <div class="debug">
         <div class="main-header">
-            <div class="logo"></div>
+            <div class="logo">
+                <img src="/images/dark-logo.png" alt="">
+            </div>
             
-            <nav class="nav"></nav>
+            <nav class="nav">
+                <ul>
+                    <li>Home 
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li>
+                    <li>Pages 
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li>                    
+                    <li>Courses 
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li> 
+                    <li>Features 
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li> 
+                    <li>Blog 
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li> 
+                    <li>Shop 
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li> 
+                    <li><i class="fa-regular fa-circle-user"></i></li>
+                </ul>
+            </nav>
             
-            <div class="input"></div>
+            <div class="input-box">
+                <input type="text" placeholder="Serch..">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
         </div>
         
     </div>
