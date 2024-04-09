8575a2499b66814ebf1dd33c75d04e81d641196e
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-04-09 18:16:44 +0200

    add TopLearners 1/2 section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -260,14 +260,63 @@ export default {
         
         <div class="topLearners">
             <div class="topLearners-head">
-                <h6>Lorem, ipsum dolor amet.</h6>
-                <h1>Lorem ipsum dolor sit amet .</h1>
+                <h5>Great words about maxcoach</h5>
+                <h1>Our </h1>
+                <h1>top learners' </h1>
+                <h1>verbatim</h1>
             </div>
 
             <div class="topLearners-reviews">
-                <div class="review re-lx"></div>
-                <div class="review re-middle"></div>
-                <div class="review re-rx"></div>
+                <div class="review re-lx">
+                    <h4>It's a choice of quality for people with special needs</h4>
+                    <p>I'm a very strict person so I require everything to be organized and neat. Then. I'll be able to make things right and shine, MaxCoach guys just got me.</p>
+
+                    <div class="reviewed-person">
+                        <img src="/images/testimonial-avata-03.jpg" alt="">
+                        <div class="review-name-box">
+                            <h4>
+                                Florence Themes
+                            </h4>
+                            <aside>/ Multimedia Admin</aside>
+                        </div>
+                        <aside></aside>
+                    </div>
+                </div>
+                <div class="review re-middle">
+                    <h4>High level of effeciency and scientific teaching methods</h4>
+                    <p> utan, free to learn and my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.</p>
+
+                    <div class="reviewed-person">
+                        <img src="/images/testimonial-avata-02.jpg" alt="">
+                        <div class="review-name-box">
+                            <h4>
+                                Mina Hollace
+                            </h4>
+                            <aside>/ Freelancer</aside>
+                        </div>
+                        <aside></aside>
+                    </div>
+                </div>
+                <div class="review re-rx">
+                    <h4>Professoinal team of specialists and passionate mentors at reach</h4>
+                    <p>I need to get a certification for English poroficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.</p>
+
+                    <div class="reviewed-person">
+                        <img src="/images/testimonial-avata-04.jpg" alt="">
+                        <div class="review-name-box">
+                            <h4>
+                                Madley Pondor
+                            </h4>
+                            <aside>/ IT Specialist</aside>
+                        </div>
+                        <aside></aside>
+                    </div>
+                </div>
+
+            </div>
+            
+            <div class="dots">
+                <i class="fa-solid fa-ellipsis"></i>
             </div>
 
             <div class="get-started">
