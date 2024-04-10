c009de8a560d7ee7e932aafc5b3595239b09cb59
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-04-10 11:10:09 +0200

    add Best Blogs section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,25 @@
 <script>
 export default {
-    name: 'AppMain'
+    name: 'AppMain',
+
+    data() {
+        return {
+            bestBlogsItems: [
+                "Become a Better Blogger: Contetnt Planning",
+                "Promoting Your Online Business on Pinterest",
+                "Gratification and GAme-Based Learning",
+                "Designing on Online Course from Experts's Perspective.",
+                "Why Online Courses Are the Future of Education"
+            ]
+        }
+    }
 }
 </script>
 
 <template>
     <div class="main debug">
 
-        <div class="jumbotron">
+        <section class="jumbotron">
             <div class="jumbo-top">
 
                 <div class="jumbo-text">
@@ -19,49 +31,50 @@ export default {
 
                     <button>
                         <i class="fa-solid fa-download"> </i>
-                          Download free guide
+                        Download free guide
                     </button>
 
-                    <aside>Have questions? 
+                    <aside>Have questions?
                         <span class="free-sample">
-                            Get Free Sample 
+                            Get Free Sample
                             <i class="fa-solid fa-arrow-right"></i>
                         </span>
                     </aside>
                 </div>
 
                 <div class="jumbo-image">
-                    <img class="main-image" src="/public/images/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="">
-                    <img class="square-dashed-up" src="/public/images/maxcoach-shape-07-100x100.png" alt="">                      
-                    <img class="square-dashed-down" src="/public/images/maxcoach-shape-07-100x100.png" alt="">
-                    <img class="square-dashed-right" src="/public/images/maxcoach-shape-07-150x150.png" alt="">                    
+                    <img class="main-image"
+                        src="/images/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="">
+                    <img class="square-dashed-up" src="/images/maxcoach-shape-07-100x100.png" alt="">
+                    <img class="square-dashed-down" src="/images/maxcoach-shape-07-100x100.png" alt="">
+                    <img class="square-dashed-right" src="/images/maxcoach-shape-07-150x150.png" alt="">
 
-                    <img class="circle-jumbo" src="/public/images/maxcoach-shape-02.png" alt="">
+                    <img class="circle-jumbo" src="/images/maxcoach-shape-02.png" alt="">
                     <i class="fa-regular fa-circle"></i>
                 </div>
 
             </div>
 
             <div class="jumbo-sponsor">
-                <img src="/public/images/client-logo-01.png" alt="">
-                <img src="/public/images/client-logo-02.png" alt="">
-                <img src="/public/images/client-logo-03.png" alt="">
-                <img src="/public/images/client-logo-04.png" alt="">
-                <img src="/public/images/client-logo-05.png" alt="">
-                <img src="/public/images/client-logo-06.png" alt="">
+                <img src="/images/client-logo-01.png" alt="">
+                <img src="/images/client-logo-02.png" alt="">
+                <img src="/images/client-logo-03.png" alt="">
+                <img src="/images/client-logo-04.png" alt="">
+                <img src="/images/client-logo-05.png" alt="">
+                <img src="/images/client-logo-06.png" alt="">
             </div>
 
             <div class="quick-access">
                 <ul class="quick-access-items">
-                    <li><img src="/public/images/set-square-drawing.svg" alt=""></li>
+                    <li><img src="/images/set-square-drawing.svg" alt=""></li>
                     <li><i class="fa-solid fa-life-ring"></i></li>
                     <li><i class="fa fa-book" aria-hidden="true"></i></li>
                     <li><i class="fa-solid fa-cart-shopping"></i></li>
                 </ul>
             </div>
-        </div>
+        </section>
 
-        <div class="FeOnCo">
+        <section class="FeOnCo">
             <div class="FeOnCo-head">
                 <h5>Stimulated to learn?</h5>
                 <h1>Featured Online Courses</h1>
@@ -182,12 +195,12 @@ export default {
                 </div>
 
             </div>
-            <button>View all courses 
+            <button>View all courses
                 <i class="fa fa-arrow-right" aria-hidden="true"></i>
             </button>
-        </div>
+        </section>
 
-        <div class="companionship">
+        <section class="companionship">
             <div class="companionship-head">
                 <h5>Maxcoach available courses</h5>
                 <h1>Access </h1>
@@ -197,25 +210,28 @@ export default {
 
             <div class="companionship-benefits">
                 <div class="benefit">
-                    <img src="/public/images/home-2-image-box-01.png" alt="">
+                    <img src="/images/home-2-image-box-01.png" alt="">
                     <h2>Customize Preferences</h2>
-                    <p>Learners are given the right to arrange and customise their study schedule and timetable based on preferences.</p>
+                    <p>Learners are given the right to arrange and customise their study schedule and timetable based on
+                        preferences.</p>
                     <a href="#">Start here
                         <i class="fa fa-arrow-right" aria-hidden="true"></i>
                     </a>
                 </div>
                 <div class="benefit">
-                    <img src="/public/images/about-us-02-image-box-02.png" alt="">
+                    <img src="/images/about-us-02-image-box-02.png" alt="">
                     <h2>Cloud Storage</h2>
-                    <p>You can store online lessons via online cloud or download  to your device and revise lessons on the way.</p>
+                    <p>You can store online lessons via online cloud or download to your device and revise lessons on
+                        the way.</p>
                     <a href="#">Get Free Quote
                         <i class="fa fa-arrow-right" aria-hidden="true"></i>
                     </a>
                 </div>
                 <div class="benefit">
-                    <img src="/public/images/home-2-image-box-03.png" alt="">
+                    <img src="/images/home-2-image-box-03.png" alt="">
                     <h2>Multimedia Channels</h2>
-                    <p>We provide study materials with various formats: video, audio, slides, doc, prints, books and applications.</p>
+                    <p>We provide study materials with various formats: video, audio, slides, doc, prints, books and
+                        applications.</p>
                     <a href="#">Start now
                         <i class="fa fa-arrow-right" aria-hidden="true"></i>
                     </a>
@@ -224,13 +240,13 @@ export default {
 
             <div class="companionship-main">
                 <div class="compan-image-box">
-                    <img class="main-image" src="/public/images/home-2-popup-video-poster.jpg" alt="">
+                    <img class="main-image" src="/images/home-2-popup-video-poster.jpg" alt="">
 
-                    <img class="yt" src="/public/images/icon-youtube-play.png" alt="">
+                    <img class="yt" src="/images/icon-youtube-play.png" alt="">
 
-                    <img class="rombo-dashed" src="/public/images/maxcoach-shape-05.png" alt="">
+                    <img class="rombo-dashed" src="/images/maxcoach-shape-05.png" alt="">
 
-                    <img class="square-dashed" src="/public/images/maxcoach-shape-07-150x150.png" alt="">
+                    <img class="square-dashed" src="/images/maxcoach-shape-07-150x150.png" alt="">
 
                     <i class="fa-solid fa-cloud"></i>
 
@@ -245,20 +261,21 @@ export default {
                     <h1 class="enjoy">Enjoy Our</h1>
                     <h1>Companionship</h1>
 
-                    <p>Sneek peek into what has in store for you on our online teaching and mentoring portal. Seek for some gidden gint of early-bird rewards as well.</p>
+                    <p>Sneek peek into what has in store for you on our online teaching and mentoring portal. Seek for
+                        some gidden gint of early-bird rewards as well.</p>
 
-                    <a href="#">Download free guidebook 
+                    <a href="#">Download free guidebook
                         <i class="fa fa-arrow-right" aria-hidden="true"></i>
                     </a>
 
                 </div>
             </div>
-        </div>
+        </section>
 
-        <img class="design-wave" src="/public/images/wave.svg" alt="">
+        <img class="design-wave" src="/images/wave.svg" alt="">
 
-        
-        <div class="topLearners">
+
+        <section class="topLearners">
             <div class="topLearners-head">
                 <h5>Great words about maxcoach</h5>
                 <h1>Our </h1>
@@ -269,7 +286,8 @@ export default {
             <div class="topLearners-reviews">
                 <div class="review re-lx">
                     <h4>It's a choice of quality for people with special needs</h4>
-                    <p>I'm a very strict person so I require everything to be organized and neat. Then. I'll be able to make things right and shine, MaxCoach guys just got me.</p>
+                    <p>I'm a very strict person so I require everything to be organized and neat. Then. I'll be able to
+                        make things right and shine, MaxCoach guys just got me.</p>
 
                     <div class="reviewed-person">
                         <img src="/images/testimonial-avata-03.jpg" alt="">
@@ -284,7 +302,8 @@ export default {
                 </div>
                 <div class="review re-middle">
                     <h4>High level of effeciency and scientific teaching methods</h4>
-                    <p> utan, free to learn and my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.</p>
+                    <p> utan, free to learn and my own pace, follow my own schedule and choose the subject I want to
+                        learn from the syllabus. Great study portal for people like me.</p>
 
                     <div class="reviewed-person">
                         <img src="/images/testimonial-avata-02.jpg" alt="">
@@ -299,7 +318,8 @@ export default {
                 </div>
                 <div class="review re-rx">
                     <h4>Professoinal team of specialists and passionate mentors at reach</h4>
-                    <p>I need to get a certification for English poroficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.</p>
+                    <p>I need to get a certification for English poroficiency and MaxCoach is my best choice. Their
+                        tutors are smart and professional when dealing with students.</p>
 
                     <div class="reviewed-person">
                         <img src="/images/testimonial-avata-04.jpg" alt="">
@@ -314,7 +334,7 @@ export default {
                 </div>
 
             </div>
-            
+
             <div class="dots">
                 <span>.</span>
                 <span>.</span>
@@ -328,13 +348,13 @@ export default {
                 <h1>You can be your own guiding star with our help!</h1>
                 <button>Get started now</button>
 
-                <img class="stripes-shape" src="/public/images/maxcoach-shape-01.png" alt="">
-                <img class="circle-shape" src="/public/images/maxcoach-shape-02.png" alt="">
+                <img class="stripes-shape" src="/images/maxcoach-shape-01.png" alt="">
+                <img class="circle-shape" src="/images/maxcoach-shape-02.png" alt="">
                 <i class="fa-regular fa-circle" aria-hidden="true"></i>
             </div>
-        </div>
+        </section>
 
-        <div class="our-numbers">
+        <section class="our-numbers">
             <div class="number">
 
             </div>
@@ -365,31 +385,55 @@ export default {
 
             <div class="number">
             </div>
-        </div>
+        </section>
 
-        <div class="best-blogs">
-            <div class="best-blog bb-text">
-                <h3>Lorem ipsum dolor sit.</h3>
-                <ul>
-                    <li>Lorem ipsum dolor sit amet .</li>
-                    <li>Lorem ipsum dolor sit amet .</li>
-                    <li>Lorem ipsum dolor sit amet .</li>
-                    <li>Lorem ipsum dolor sit amet .</li>
-                </ul>
-            </div>
-            <div class="best-blog">
-                <img src="" alt="">
-            </div>
-            <div class="best-blog">
-                <img src="" alt="">
-            </div>
-        </div>
+        <section class="best-blogs">
 
-        <div class="subscribe">
+            <div class="blogs-box">
+                <div class="best-blog bb-text">
+                    <h3>Enjoyable insights</h3>
+                    <h1>Most Viewed </h1>
+                    <h1>Best Blogs</h1>
+                    <ul>
+                        <li v-for="bbItem in bestBlogsItems">
+                            <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                            {{ bbItem }}</li>
+                        </ul>
+                    </div>
+                    
+                    <div class="best-blog">
+                        <img src="/images/artist-blog-03-480x352.jpeg" alt="">
+
+                        <h3>Artist</h3>
+                        <h4>Brush Strokes Energize Trees in Paintings</h4>
+
+                        <span><i class="fa fa-calendar" aria-hidden="true"></i>
+                        May, 15, 2020
+                        </span>                     <span><i class="fa fa-eye" aria-hidden="true"></i>
+                        688 views
+                        </span>
+                    </div>
+
+                    <div class="best-blog">
+                        <img src="/images/artist-blog-01-480x352.jpg" alt="">
+
+                        <h3>Artist</h3>
+                        <h4>Pocket-Sized Notebooks Hold Miniature Paintings</h4>
+
+                        <span><i class="fa fa-calendar" aria-hidden="true"></i>
+                        May, 15, 2020
+                        </span>                     <span><i class="fa fa-eye" aria-hidden="true"></i>
+                        603 views
+                        </span>
+                    </div>
+                </div>
+        </section>
+
+        <section class="subscribe">
             <div class="subscribe-head"></div>
             <input type="text">
             <button>Lorem.</button>
-        </div>
+        </section>
 
     </div>
 </template>
